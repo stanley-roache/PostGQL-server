bc6bc4d5e45d782b024bfa84e28413a8f09a386a
stanley-roache <stan.roache@gmail.com>
2018-11-07 22:12:25 +1300

    docker compose method works with only the database, port 2345 exposed, postgraphile connection to db isnt working yet

--- db/init/02_initialise.sql
+++ db/init/02_initialise.sql
@@ -5,17 +5,17 @@
 -- Dumped from database version 10.5 (Debian 10.5-1)
 -- Dumped by pg_dump version 10.5 (Debian 10.5-1)
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
+-- SET statement_timeout = 0;
+-- SET lock_timeout = 0;
+-- SET idle_in_transaction_session_timeout = 0;
+-- SET client_encoding = 'UTF8';
+-- SET standard_conforming_strings = on;
+-- SELECT pg_catalog.set_config('search_path', '', false);
+-- SET check_function_bodies = false;
+-- SET client_min_messages = warning;
+-- SET row_security = off;
 
-DROP DATABASE centripetal;
+-- DROP DATABASE centripetal;
 --
 -- Name: centripetal; Type: DATABASE; Schema: -; Owner: cp_postgraphile
 --
